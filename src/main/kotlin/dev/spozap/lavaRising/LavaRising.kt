bce80ce36c45c17e0bd3b981c687f8ef15486141
spozap <spozapdev@gmail.com>
2025-09-08 22:02:01 +0200

    first commit

---
+++ src/main/kotlin/dev/spozap/lavaRising/LavaRising.kt
@@ -0,0 +1,14 @@
+package dev.spozap.lavaRising
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class LavaRising : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
